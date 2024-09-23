1faf04c4f99ff42555743580355a8ceddaa3144c
Ashraf Azad <ashrafazad86@gmail.com>
2024-09-23 19:41:13 +0600

    donation system integrated

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-console.log('home js connected')
+console.log("home js connected");
+
+document
+  .getElementById("btn-donate-noakhali")
+  .addEventListener("click", function () {
+    donateMoney("donate-noakhali-flood", "balance-noakhali");
+  });
+document
+  .getElementById("btn-donate-feni")
+  .addEventListener("click", function () {
+    donateMoney("donate-feni-flood", "balance-feni");
+  });
+document
+  .getElementById("btn-donate-quotaMovement")
+  .addEventListener("click", function () {
+    donateMoney("donate-quota-movement", "balance-quota-movement");
+  });
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,24 @@
 console.log("utilities js connected");
+
+function donateMoney(amount, fundName) {
+  const balanceString = document.getElementById("balance").innerText;
+  const balance = Number(balanceString);
+  const donationString = document.getElementById(amount).value;
+  const donationAmount = Number(donationString);
+  const fundBalanceString = document.getElementById(fundName).innerText;
+  const fundBalance = Number(fundBalanceString);
+  if (
+    !isNaN(donationAmount) &&
+    donationAmount <= balance &&
+    donationAmount > 0
+  ) {
+    document.getElementById(fundName).innerText = fundBalance + donationAmount;
+    document.getElementById("balance").innerText = balance - donationAmount;
+    console.log("condition passed");
+    document.getElementById(amount).value = null;
+    document.getElementById("my_modal_5").showModal();
+  } else {
+    document.getElementById(amount).value = null;
+    alert("Unable to donate! Please Check Your Input");
+  }
+}
